a4b282a37e0dfe65dd7b6dd9e59fbd664562e3f9
gueverstarstone <josephmungai028@gmail.com>
2025-11-25 21:18:02 +0300

    worked on changes to different components and pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,15 @@ import styles from "./home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Welcome to skillConnect</h1>
+      <h1 className={styles.titleSpan}>
+        Connect with Skilled Professionals Instantly
+      </h1>
+      <p>
+        Discover trusted, verified professionals from every occupation across
+        Kenya. Skill Connect makes it easy to find the right expert, build
+        strong teams, or grow your own career—all with confidence and
+        transparency.
+      </p>
       <p className={styles.subtitle}>Select how you'd like to continue:</p>
 
       <div className={styles.roles}>
