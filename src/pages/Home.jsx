360bfc06989f0ae950628f1a734b757a40507813
gueverstarstone <josephmungai028@gmail.com>
2025-11-25 15:48:21 +0300

    styled the home page and added links to workers and admin panel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>home page</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Welcome to skillConnect</h1>
+      <p className={styles.subtitle}>Select how you'd like to continue:</p>
+
+      <div className={styles.roles}>
+        <Link to="/workers" className={styles.roleBtn}>
+          Find Workers
+        </Link>
+
+        <Link to="/admin" className={`${styles.roleBtn} ${styles.admin}`}>
+          Admin Panel
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
